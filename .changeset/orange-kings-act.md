b80a2a9f543f9d1818175f2c33fa753a4fa69077
catapart <catapart@outlook.com>
2024-11-30 13:54:14 -0600

    updated list layout style to expand to task-card width; updated demo page ui;

---
+++ .changeset/orange-kings-act.md
@@ -0,0 +1,5 @@
+---
+"@magnit-ce/task-board": patch
+---
+
+updated list layout style to expand to task-card width
--- vanilla/task-board.js
+++ vanilla/task-board.js
@@ -12,13 +12,14 @@ COMPONENT_STYLESHEET.replaceSync(`
 {
     display: grid;
     grid-auto-flow: column;
-    grid-auto-columns: minmax(var(--list-min-width), 1fr);
+    grid-auto-columns: max-content;
     flex: 1;
     overflow: auto hidden;
 }
 ::slotted(task-list)
 {
     overflow: hidden auto;
+    min-width: var(--list-min-width);
 }
 `);
 var COMPONENT_TAG_NAME = "task-board";
